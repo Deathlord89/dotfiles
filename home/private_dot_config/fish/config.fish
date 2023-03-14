6474d87a2d9cab877039e795e7c82da9e85a720a
Marc-André Gerbig <marc.gerbig@gmail.com>
2023-03-14 19:50:35 +0100

    Fix gpg config error

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -1,3 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -x GPG_TTY (tty)
+    set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    gpgconf --launch gpg-agent
+end
+
 ## Source custom configs
 for file in $__fish_config_dir/custom.d/*.fish
   source $file
