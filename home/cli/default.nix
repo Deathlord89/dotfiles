17e574ed44f5858ccd81b06435d682867c06e2ad
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-15 12:46:57 +0100

    fix(home-manager): reload font cache
    copy-paste error

--- home/cli/default.nix
+++ home/cli/default.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   libx,
   pkgs,
   ...
@@ -39,12 +38,4 @@
     wl-clipboard # Wayland clipboard
     xclip # X11 clipboard
   ];
-
-  # Reload font cache on rebuild to avoid issues similar to
-  # https://www.reddit.com/r/NixOS/comments/1kwogzf/after_moving_to_2505_system_fonts_no_longer/
-  activation.reloadFontCache = lib.hm.dag.entryAfter [ "linkActivation" ] ''
-    if [ -x "${pkgs.fontconfig}/bin/fc-cache" ]; then
-      ${pkgs.fontconfig}/bin/fc-cache -f
-    fi
-  '';
 }
