ff0714be832915b7c6e1853dac5d654dc34bd803
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-15 18:29:24 +0100

    feat(hosts): add restic module

---
+++ hosts/common/optional/restic.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.backup.restic;
+in
+{
+  options.backup.restic = {
+    enable = lib.mkEnableOption "enable restic jobs";
+  };
+
+  config = lib.mkIf cfg.enable {
+    sops.secrets = {
+      "restic_pass" = { };
+      "rclone_conf" = {
+        path = "/root/.config/rclone/rclone.conf";
+      };
+    };
+
+    environment.systemPackages = with pkgs; [
+      restic
+      rclone
+    ];
+  };
+}
