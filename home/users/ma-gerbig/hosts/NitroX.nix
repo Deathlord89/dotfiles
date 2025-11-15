8ba234518acb031502304e86639a9f0cc158df2c
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-15 21:05:04 +0100

    feat(NitroX): enable optional services

---
+++ home/users/ma-gerbig/hosts/NitroX.nix
@@ -0,0 +1,11 @@
+{
+  optional = {
+    dev.vscode.enable = true;
+    optional.games = {
+      lutris.enable = true;
+      mangohud.enable = true;
+      prismlauncher.enable = true;
+    };
+    productivity.vivaldi.enable = true;
+  };
+}
