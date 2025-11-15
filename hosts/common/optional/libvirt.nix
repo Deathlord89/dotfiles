d1836753b5b3ca91869bab237724c638275b0af5
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-15 18:29:39 +0100

    feat(hosts): add libvirt module

---
+++ hosts/common/optional/libvirt.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.optional.libvirt;
+in
+{
+  options.optional.libvirt = {
+    enable = lib.mkEnableOption "Enable Libvirt Service";
+  };
+
+  config = lib.mkIf cfg.enable {
+    virtualisation = {
+      libvirtd = {
+        enable = true;
+        qemu = {
+          package = pkgs.qemu_kvm;
+          swtpm.enable = true;
+        };
+      };
+      spiceUSBRedirection.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      spice
+      spice-gtk
+      spice-protocol
+      virt-viewer
+      virtio-win
+      win-spice
+    ];
+    programs.virt-manager.enable = true;
+
+    networking.firewall.trustedInterfaces = [ "virbr0" ];
+  };
+}
