6ff275c967f1533931850eac98ba2ad03f9381c1
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-15 23:45:40 +0100

    feat(stylix): enable stylix integration for kitty

--- home/desktop/core/kitty.nix
+++ home/desktop/core/kitty.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   icon = pkgs.fetchurl {
     url = "https://github.com/DinkDonk/kitty-icon/blob/main/kitty-dark.png?raw=true";
@@ -6,21 +11,27 @@ let
   };
 in
 {
-  xdg.dataFile."icons/hicolor/256x256/apps/kitty.png".source = icon;
-  xdg.dataFile."icons/hicolor/scalable/apps/kitty.png".source = icon;
+  config = {
+    xdg.dataFile."icons/hicolor/256x256/apps/kitty.png".source = icon;
+    xdg.dataFile."icons/hicolor/scalable/apps/kitty.png".source = icon;
 
-  programs.kitty = {
-    enable = true;
-    settings = {
-      allow_remote_control = "no";
-      confirm_os_window_close = "-1";
-      disable_ligatures = "cursor";
-      remember_window_size = "no";
-      strip_trailing_spaces = "smart";
-      term = "xterm-256color";
-      window_padding_width = "2";
-      # OS specific
-      linux_display_server = "x11";
+    programs.kitty = {
+      enable = true;
+      settings = {
+        allow_remote_control = "no";
+        confirm_os_window_close = "-1";
+        disable_ligatures = "cursor";
+        remember_window_size = "no";
+        strip_trailing_spaces = "smart";
+        term = "xterm-256color";
+        window_padding_width = "2";
+        # OS specific
+        linux_display_server = "x11";
+      };
+    };
+
+    stylix.targets.kitty = lib.mkIf config.optional.stylix.enable {
+      enable = true;
     };
   };
 }
