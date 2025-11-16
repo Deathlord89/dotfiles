9b4816d095fc853cf766c955a3e3d5c918b9512a
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-16 19:21:20 +0100

    feat(home-manager): enable stylix support for zellij

--- home/cli/zellij.nix
+++ home/cli/zellij.nix
@@ -1,5 +1,14 @@
 {
+  config,
+  lib,
+  ...
+}:
+{
   programs.zellij = {
+    enable = true;
+  };
+
+  stylix.targets.zellij = lib.mkIf config.optional.stylix.enable {
     enable = true;
   };
 }
