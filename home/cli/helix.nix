10bfff33449a18b429293ad2247c6d03ad2acc8b
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-17 21:57:03 +0100

    feat(home-manager): add markdown lsp support for helix

--- home/cli/helix.nix
+++ home/cli/helix.nix
@@ -11,12 +11,25 @@
     };
     languages.language = [
       {
+        name = "markdown";
+        auto-format = true;
+        language-servers = [
+          "marksman"
+          "ltex-ls-plus"
+        ];
+      }
+      {
         name = "nix";
         auto-format = true;
         formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
       }
     ];
     extraPackages = with pkgs; [
+      # Markdown
+      marksman
+      ltex-ls-plus
+
+      # Nix
       nixd
       nixfmt-rfc-style
     ];
