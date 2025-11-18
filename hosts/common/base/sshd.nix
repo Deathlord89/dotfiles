bd719d39f34e43feb30a87c028949956db3b60ad
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-18 22:54:03 +0100

    fix(openssh): set file permissions for ssh keys

--- hosts/common/base/sshd.nix
+++ hosts/common/base/sshd.nix
@@ -61,5 +61,8 @@
 
   systemd.tmpfiles.rules = [
     "z /etc/ssh/ssh_host_ed25519_key 600 root root - -"
+    "z /etc/ssh/ssh_host_ed25519_key.pub 0644 root root - -"
+    "z /etc/ssh/ssh_host_rsa_key 600 root root - -"
+    "z /etc/ssh/ssh_host_rsa_key.pub_key.pub 0644 root root - -"
   ];
 }
