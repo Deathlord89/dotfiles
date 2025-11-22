d8879c8e86e222b2a29ca0f9e7d08004cb7dc436
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-22 21:26:38 +0100

    feat: add podman global module

---
+++ hosts/common/optional/podman.nix
@@ -0,0 +1,33 @@
+{
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+      # Required for container networking to be able to use names.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  ### Enable Auto-Update
+  systemd.services.podman-auto-update = {
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.timers.podman-auto-update = {
+    enable = true;
+    description = "Periodic Podman container auto-update";
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
+
+  networking.firewall.interfaces.podman1 = {
+    allowedUDPPorts = [ 53 ]; # this needs to be there so that containers can look eachother's names up over DNS
+  };
+}
