a2e9299e8e57b45b74f73e364311ad567ba3415f
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-29 23:45:23 +0100

    fix(ghostty): fix for `Dead keys stopped working since GNOME 49 / GTK 4.20`
    See also: https://github.com/ghostty-org/ghostty/discussions/8899

--- home/desktop/core/ghostty.nix
+++ home/desktop/core/ghostty.nix
@@ -6,6 +6,10 @@
 {
   programs.ghostty.enable = true;
 
+  # FIX
+  # See: https://github.com/ghostty-org/ghostty/discussions/8899
+  home.sessionVariables.GTK_IM_MODULE = "simple";
+
   stylix.targets.ghostty = lib.mkIf config.optional.stylix.enable {
     enable = true;
   };
