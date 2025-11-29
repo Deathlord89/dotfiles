a7805fae001a76de3f397ec5b44330b28716b1f5
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-11-29 18:48:44 +0100

    feat(autoUpgrade) enable autoUpgrade for nonserver hosts

--- hosts/NitroX/extra.nix
+++ hosts/NitroX/extra.nix
@@ -1,7 +1,13 @@
+{ config, ... }:
 {
   backup.restic.enable = true;
 
   optional = {
+    autoUpgrade = {
+      enable = true;
+      user = config.users.users.ma-gerbig.name;
+      flakeDir = "${config.users.users.ma-gerbig.home}/.dotfiles";
+    };
     flatpak.enable = true;
     gaming.enable = true;
     libvirt.enable = true;
--- hosts/T460p/extra.nix
+++ hosts/T460p/extra.nix
@@ -1,5 +1,11 @@
+{ config, ... }:
 {
   optional = {
+    autoUpgrade = {
+      enable = true;
+      user = config.users.users.ma-gerbig.name;
+      flakeDir = "${config.users.users.ma-gerbig.home}/.dotfiles";
+    };
     flatpak.enable = true;
     gaming.enable = true;
     secureboot.enable = true;
