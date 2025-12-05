0823ee9281cbed9d13fe38eca1fa921d31e38c9d
Marc-André Gerbig <marc.gerbig@gmail.com>
2025-12-05 13:35:40 +0100

    fix(nextcloud): bump to major version 32

--- hosts/NAZGUL/services/nextcloud.nix
+++ hosts/NAZGUL/services/nextcloud.nix
@@ -30,7 +30,7 @@
   services = {
     nextcloud = {
       enable = true;
-      package = pkgs.nextcloud31;
+      package = pkgs.nextcloud32;
       hostName = "cloud.${inputs.nix-secrets.domain}";
       # home dir is default: /var/lib/nextcloud
       datadir = "/var/cloud";
