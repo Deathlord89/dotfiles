5f661f16f4d9083448562943adb5df19b378aaae
Marc-André Gerbig <marc.gerbig@gmail.com>
2026-01-11 20:48:54 +0100

    feat(jellyfin): add collection manager to jellyfin

--- hosts/NAZGUL/services/jellyfin.nix
+++ hosts/NAZGUL/services/jellyfin.nix
@@ -19,6 +19,9 @@ in
     "jdownloader/env.enc" = {
       restartUnits = [ "podman-jdownloader2.service" ];
     };
+    "umlautadaptarr/env.enc" = {
+      restartUnits = [ "podman-umlautadaptarr.service" ];
+    };
   };
 
   services = {
@@ -27,10 +30,30 @@ in
       group = mediaGroup;
       openFirewall = true;
     };
+
     sabnzbd = {
       enable = true;
       inherit (config.services.jellyfin) user;
       group = mediaGroup;
+    };
+
+    sonarr = {
+      enable = true;
+      inherit (config.services.jellyfin) user;
+      group = mediaGroup;
+      openFirewall = true;
+    };
+
+    radarr = {
+      enable = true;
+      inherit (config.services.jellyfin) user;
+      group = mediaGroup;
+      openFirewall = true;
+    };
+
+    prowlarr = {
+      enable = true;
+      openFirewall = true;
     };
   };
 
@@ -42,6 +65,24 @@ in
   optional.podman.enable = true;
   virtualisation.oci-containers = {
     containers = {
+      "umlautadaptarr" = {
+        image = "docker.io/pcjones/umlautadaptarr";
+        autoStart = true;
+        environment = {
+          TZ = "Europe/Berlin";
+          SONARR__ENABLED = "true";
+          SONARR__HOST = "http://192.168.10.10:8989";
+          RADARR__ENABLED = "true";
+          RADARR__HOST = "http://192.168.10.10:7878";
+        };
+        environmentFiles = [ "${config.sops.secrets."umlautadaptarr/env.enc".path}" ];
+        ports = [ "5006:5006/tcp" ];
+        labels = {
+          "io.containers.autoupdate" = "registry";
+        };
+        log-driver = "journald";
+      };
+
       "jdownloader2" = {
         image = "docker.io/jaymoulin/jdownloader";
         autoStart = true;
